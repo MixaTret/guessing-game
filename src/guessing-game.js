f50be9f1773f4d3f25b515208963093458206cfb
MixaTret <mikhail.tretsiakevich@gmail.com>
2023-10-19 17:19:06 +0100

    feat: all tests were done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.newMin = min;
+        this.newMax = max;
     }
 
     guess() {
-
+        this.requiredNumber = Math.ceil((this.newMin + this.newMax) / 2);
+        return this.requiredNumber;
     }
 
     lower() {
-
+        this.newMax = this.requiredNumber;
     }
 
     greater() {
-
+        this.newMin = this.requiredNumber;
     }
 }
 
